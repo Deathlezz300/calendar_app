cd7651db3b868575c180f18da0e070cd6c571a3b
Alejandro <jairoalejandro2003@gmail.com>
2024-12-19 22:44:11 -0500

    update get env variables

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -2,7 +2,7 @@ export const getEnvVariables=()=>{
 
     //const VITE_API_URL='https://calenderappbackend300.up.railway.app/api'
 
-     const VITE_API_URL='http://localhost:4000/api';
+     const VITE_API_URL=import.meta.env.VITE_API_URL;
      const MODE='';
 
     return {
